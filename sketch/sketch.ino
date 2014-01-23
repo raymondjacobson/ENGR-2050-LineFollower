de30eda507e003ce5cc72f40693428ac97cfa784
Raymond Jacobson <raymondshujacobson8@gmail.com>
2014-01-22 20:07:17 -0500

    test motors

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,7 +1,12 @@
-void setup() {
-  
+int motorA = 5;
+int motorB = 6;
+
+void setup(){
+  pinMode(motorA, OUTPUT);
+  pinMode(motorB, OUTPUT);
 }
 
 void loop() {
-  
+  digitalWrite(motorA, HIGH);
+  digitalWrite(motorB, HIGH);
 }

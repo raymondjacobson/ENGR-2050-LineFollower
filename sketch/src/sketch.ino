f8bebec60ff809191fda6996190dc20444fb66ad
Raymond Jacobson <raymondshujacobson8@gmail.com>
2014-01-31 15:50:18 -0500

    working code...

--- sketch/src/sketch.ino
+++ sketch/src/sketch.ino
@@ -1,12 +1,97 @@
-int motorA = 5;
-int motorB = 6;
+/* MOTOR CONFIGURATION */
+int leftMotor1 = 3;
+int leftMotor2 = 5;
+int rightMotor1 = 6;
+int rightMotor2 = 9;
+
+/* INPUT CONFIGURATION */
+int pushbutton = 2;
+int lastPBState = LOW;
+bool on = false;
+
+/* VEHICLE CONFIGURATION */
+int speed = 200; // out of 255
+int leftSpeed;
+int rightSpeed;
+
+int leftPhotoTransistor = A3;
+int rightPhotoTransistor = A2;
 
 void setup(){
-  pinMode(motorA, OUTPUT);
-  pinMode(motorB, OUTPUT);
+  Serial.begin(9600);
+  pinMode(leftMotor1, OUTPUT);
+  pinMode(leftMotor2, OUTPUT);
+  pinMode(rightMotor1, OUTPUT);
+  pinMode(rightMotor2, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(motorA, HIGH);
-  digitalWrite(motorB, HIGH);
+/* HELPER FUNCTIONS */
+
+void startstop(){
+  on = !on;
+}
+
+int aInToOut(int number){
+  return (((number*40)/1024.0)*255);
+}
+
+void forward(int speed){
+  analogWrite(leftMotor2, speed);
+  analogWrite(leftMotor1, 0);
+  analogWrite(rightMotor2, speed);
+  analogWrite(rightMotor1, 0);
+}
+
+void adjustedForward(int leftSpeed, int rightSpeed){
+  analogWrite(leftMotor2, leftSpeed);
+  analogWrite(leftMotor1, 0);
+  analogWrite(rightMotor2, rightSpeed);
+  analogWrite(rightMotor1, 0);
+}
+
+void reverse(int speed){
+  analogWrite(leftMotor1, speed);
+  analogWrite(leftMotor2, 0);
+  analogWrite(rightMotor1, speed);
+  analogWrite(rightMotor2, 0);
+}
+
+void left(int speed){
+  analogWrite(leftMotor2, speed);
+  analogWrite(leftMotor1, 0);
+  analogWrite(rightMotor2, 0);
+  analogWrite(rightMotor1, speed);
+}
+
+void right(int speed){
+  analogWrite(leftMotor2, 0);
+  analogWrite(leftMotor1, speed);
+  analogWrite(rightMotor2, speed);
+  analogWrite(rightMotor1, 0);
+}
+
+void stop(){
+  analogWrite(leftMotor1, 0);
+  analogWrite(leftMotor2, 0);
+  analogWrite(rightMotor1, 0);
+  analogWrite(rightMotor2, 0);
+}
+
+void loop(){
+  int pbResponse = digitalRead(pushbutton);
+  if (pbResponse == HIGH && lastPBState == LOW){
+    startstop();
+  }
+  lastPBState = pbResponse;
+
+  leftSpeed = aInToOut(analogRead(leftPhotoTransistor));
+  rightSpeed = aInToOut(analogRead(rightPhotoTransistor));
+  // Serial.println(leftSpeed);
+  if (on){
+    adjustedForward(leftSpeed, rightSpeed);
+    // forward(speed);
+  }
+  else {
+    stop();
+  }
 }
